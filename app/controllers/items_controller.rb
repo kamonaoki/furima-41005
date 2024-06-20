d3e8daf89536187921eb0dfb6c15873f43b964e1
kamonaoki <naokun616@gmail.com>
2024-06-20 15:46:04 +0900

    コントローラー内容編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,16 +4,27 @@ class ItemsController < ApplicationController
 
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
+  def item_params
+    params.require(:item).permit(:item_name, :price, :detail,:category_id, :condition_id, :burden_id, :prefecture_id, :delivery_date_id)
+  end
 
   def set_item
     @item = Item.find(params[:id])
