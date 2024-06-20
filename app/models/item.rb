1714756f02723c21519e3bacb3dca025114c74c2
kamonaoki <naokun616@gmail.com>
2024-06-20 15:39:58 +0900

    アクティブハッシュファイル（仮）準備＋バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :delivery_date
+
+  validates :item_name, :price, :detail, presence: true
+  validates :category_id, :condition_id, :burden_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 1 } 
 end
