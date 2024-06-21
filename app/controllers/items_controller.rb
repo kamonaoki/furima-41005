dc37e60f01fe343873b0539e8441badfc6858aa4
kamonaoki <naokun616@gmail.com>
2024-06-21 16:52:07 +0900

    コントローラーに記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
-
   before_action :move_to_index, except: [:index, :show]
 
 
   def index
-  
+    @items = Item.order("created_at DESC")
   end
 
   def new
