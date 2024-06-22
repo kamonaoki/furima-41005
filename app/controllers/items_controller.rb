efd0d8e065d68b1e7d76d94343027997707a80b0
kamonaoki <naokun616@gmail.com>
2024-06-22 15:53:07 +0900

    その商品の出品者じゃなければ、編集ページに直接行ってもトップページに戻されるように

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,8 +48,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if user_signed_in?
-
-    redirect_to action: :index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
+  
 end
