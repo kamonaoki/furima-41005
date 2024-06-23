1d7d73ec219a82f27d1e63b2ea2985362949e28a
kamonaoki <naokun616@gmail.com>
2024-06-23 15:54:03 +0900

    バリデーション作成

--- app/models/order_send.rb
+++ app/models/order_send.rb
@@ -1,5 +1,16 @@
 class OrderSend
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   include ActiveModel::Model
   attr_accessor  :post_code, :prefecture_id, :city, :street_address, :building_name, :telephone, :order_id, :item_id, :user_id     
-        
+  
+  
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :city, :street_address, presence: true
+  with_options presence: true do
+    validates :post_code,format: { with:  /^\d{3}-\d{4}$/, message: 'エラー' }
+    validates :telephone,format: { with:/\A\d{10,11}+\z/, message: 'エラー' }
+
 end
