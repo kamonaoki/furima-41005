899ad3346678979c69cf7f889d155c99f8c31cd4
kamonaoki <naokun616@gmail.com>
2024-06-23 14:03:18 +0900

    テーブル作成

--- db/migrate/20240623045017_create_sends.rb
+++ db/migrate/20240623045017_create_sends.rb
@@ -1,7 +1,13 @@
 class CreateSends < ActiveRecord::Migration[7.0]
   def change
     create_table :sends do |t|
-
+      t.string         :post_code,              null: false
+      t.string         :prefecture_id,          null: false
+      t.string         :city,                   null: false
+      t.string         :street_address,         null: false
+      t.string         :building_name 
+      t.string         :telephone,              null: false
+      t.references     :order,                  null: false,foreign_key: true 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_20_060814) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_23_045017) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,28 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_20_060814) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "orders", charset: "utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "sends", charset: "utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.string "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "street_address", null: false
+    t.string "building_name"
+    t.string "telephone", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_sends_on_order_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -75,4 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_20_060814) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
+  add_foreign_key "sends", "orders"
 end
