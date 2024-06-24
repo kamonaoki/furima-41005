0fa95478561235081831a42698086dcb1efbac14
kamonaoki <naokun616@gmail.com>
2024-06-24 15:21:29 +0900

    ログイン状態の場合でも、自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移すること。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
-
+  before_action :move_to_index, only:[:index, :create]
   def index
     @order_address = OrderAddress.new
   end
@@ -24,4 +24,10 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :street_address, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end 
 end
