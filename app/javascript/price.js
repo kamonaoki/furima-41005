89ad1823d78c24752506f6c0aa8f159ca320b2d3
kamonaoki <naokun616@gmail.com>
2024-06-26 11:30:09 +0900

    指摘点修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,8 +5,9 @@ function post (){
     const addTaxDom = document.getElementById('add-tax-price');
     addTaxDom.innerHTML = Math.floor(inputValue/10)
     const salesProfit= document.getElementById('profit');
-    salesProfit.innerHTML = Math.floor(inputValue*0.9)
+    salesProfit.innerHTML =inputValue -  Math.floor(inputValue/10)
   });
  };
  
  window.addEventListener('turbo:load', post);
+ window.addEventListener("turbo:render", post);
